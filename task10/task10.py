ceb47f64ae94a8b0befc9d676092ba718803a696
Tatiana Sheleh <apsua@tut.by>
2023-03-12 19:57:45 +0300

    решение10

--- task10/task10.py
+++ task10/task10.py
@@ -4,22 +4,22 @@
 # чтобы все монетки были повернуты вверх одной и той же стороной.
 # Выведите минимальное количество монет, которые нужно перевернуть
 
-n=int(input ("Введите кол-во монеток: "))
-moneys =[]
-for i in range (n):
-    money=int (input(f'money_{i+1}--> '))
+n = int(input("Введите кол-во монеток: "))
+moneys = []
+for i in range(n):
+    money = int(input(f'money_{i+1}--> '))
     moneys.append(money)
 # print(moneys)
-len_0=0
-len_1=0
+len_0 = 0
+len_1 = 0
 for money in moneys:
-    if money>0:
-        len_1+=1
+    if money > 0:
+        len_1 += 1
     else:
-        len_0+=1
+        len_0 += 1
 # print (len_0)
 # print (len_1)
-if len_1>len_0:
-    print (len_0)
+if len_1 > len_0:
+    print(len_0)
 else:
-    print (len_1)
+    print(len_1)
